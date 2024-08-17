9b573a8abb7fe5756a489d6fe8df6ed2a5be175d
Konstantinos <kostas.kollias@spotawheel.com>
2024-08-18 00:21:53 +0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
+use App\Http\Controllers\Authentication\LoginController;
+use App\Http\Controllers\Authentication\RegistrationController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LocalizationController;
+use App\Http\Controllers\RolesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
+Route::middleware('localization')->group(function () {
+
+    Route::middleware('auth')->prefix('users')->group( function () {
+        Route::get('/index', [UserController::class, 'index'])->name('user.index');
+        Route::post('/store', [UserController::class, 'store'])->name('user.store');
+        Route::get('/edit/{user}', [UserController::class, 'edit'])->name('user.edit');
+        Route::post('/update/{user}', [UserController::class, 'update'])->name('user.update');
+        Route::delete('/delete/{user}', [UserController::class, 'destroy'])->name('user.delete');
+        Route::post('/getData', [App\Http\Controllers\Async\UserController::class, 'getDatatableData'])->name('users.getDatatableData');
+    });
+
+    Route::middleware('auth')->prefix('roles')->group( function () {
+        Route::get('/index', [RolesController::class, 'index'])->name('role.index');
+        Route::post('/store', [RolesController::class, 'store'])->name('role.store');
+        Route::get('/edit/{role}', [RolesController::class, 'edit'])->name('role.edit');
+        Route::post('/update/{role}', [RolesController::class, 'update'])->name('role.update');
+        Route::delete('/delete/{role}', [RolesController::class, 'destroy'])->name('role.delete');
+        Route::post('/getData', [App\Http\Controllers\Async\RoleController::class, 'getDatatableData'])->name('roles.getDatatableData');
+    });
+
+    Route::get('/', [HomeController::class, 'home'])->name('home');
+
+    Route::get('/localization/{locale}', LocalizationController::class )->name('localization');
+    Route::get('/login', [LoginController::class, 'login'])->name('login');
+    Route::post('/authenticate', [LoginController::class, 'authenticate'])->name('user.authenticate');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('user.logout');
+    Route::get('/registration', [RegistrationController::class, 'registration'])->name('user.registration');
+    Route::post('/register', [RegistrationController::class, 'register'])->name('user.register');
+
 });
